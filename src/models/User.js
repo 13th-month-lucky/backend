92c7e172a916fd4eea41cee4dee260da3b585e89
seungtoctoc <102455571+seungtoctoc@users.noreply.github.com>
2024-03-20 14:22:40 +0900

    feat: User 스키마에 liedEtf, likedFund 추가

--- src/models/User.js
+++ src/models/User.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   nickname: { type: String },
   resultArray: [{ type: Number }],
+  likedEtf: [],
+  likedFund: [],
 });
 
 const User = mongoose.model("User", userSchema);
