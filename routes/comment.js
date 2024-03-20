0075a79a155e7514f2dfe91715f6cd5d923dbc32
seungtoctoc <102455571+seungtoctoc@users.noreply.github.com>
2024-03-20 14:42:58 +0900

    feat: Comment 생성, replyIds에 put

--- routes/comment.js
+++ routes/comment.js
@@ -15,4 +15,38 @@ router.get("/:code", async function (req, res, next) {
   }
 });
 
+router.post("/", async function (req, res, next) {
+  try {
+    const { nickname, profileImageUrl, code, depth, content } = req.body;
+
+    const result = await Comment.create({
+      nickname: nickname,
+      profileImageUrl: profileImageUrl,
+      code: code,
+      depth: depth,
+      content: content,
+    });
+
+    res.send(result);
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+router.put("/reply", async function (req, res, next) {
+  try {
+    const { commentId, replyId } = req.body;
+
+    const result = await Comment.findByIdAndUpdate(
+      commentId,
+      { $push: { replyIds: replyId } },
+      { new: true }
+    );
+
+    res.send(result);
+  } catch (err) {
+    res.send(err);
+  }
+});
+
 module.exports = router;
