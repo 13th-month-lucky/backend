673a827dfe791de90d8bd04786ff642b1221cc11
allllfo <mirae4292@gmail.com>
2024-03-21 13:38:18 +0900

    feat : etf차트,정보 전체 조회 기능

--- routes/etf.js
+++ routes/etf.js
@@ -7,10 +7,23 @@ const EtfChart = require("../src/models/EtfChart");
 const EtfInfo = require("../src/models/EtfInfo");
 
 //etf 종목의 정보 하나만 조회 - public
-router.get(":stockCode/info", function (req, res, next) {
+router.get("/:stockCode/info", function (req, res, next) {
   const stockCode = req.params.stockCode;
 
   EtfInfo.find({ code: stockCode })
+    .then((etf) => {
+      res.json(etf);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+//Etf정보 전체 조회
+router.get("/info", function (req, res, next) {
+  const stockCode = req.params.stockCode;
+
+  EtfInfo.find()
     .then((etf) => {
       res.json(etf);
     })
@@ -24,6 +37,19 @@ router.get("/:stockCode/chart", function (req, res, next) {
   const stockCode = req.params.stockCode;
 
   EtfChart.find({ code: stockCode })
+    .then((etf) => {
+      res.json(etf);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+//etf 차트 전체 조회
+router.get("/chart", function (req, res, next) {
+  const stockCode = req.params.stockCode;
+
+  EtfChart.find()
     .then((etf) => {
       res.json(etf);
     })
