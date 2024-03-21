1127b453f9d2a289fd74d1a12ec1133a4b04b1d4
youhyeoneee <khukyh2018@khu.ac.kr>
2024-03-21 10:07:11 +0900

    feat: 펀드 코드로 조회 API 추가

--- routes/fund.js
+++ routes/fund.js
@@ -13,4 +13,17 @@ router.get("/", function (req, res, next) {
     });
 });
 
+// 펀드 코드로 조회
+router.get("/:fundCode", function (req, res, next) {
+  Fund.findOne({ code: req.params.fundCode })
+    .then((data) => {
+      if (!data)
+        return res.status(404).json({ message: "펀드를 찾을 수 없습니다." });
+      res.json(data);
+    })
+    .catch((err) => {
+      return next(err);
+    });
+});
+
 module.exports = router;
