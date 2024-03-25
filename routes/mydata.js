4156352252075791d842fffe8876e406deb4397b
seungtoctoc <102455571+seungtoctoc@users.noreply.github.com>
2024-03-25 10:48:20 +0900

    fix: 낸 세율이 0.1에서 0.3 사이 랜덤값이 되도록 수정

--- routes/mydata.js
+++ routes/mydata.js
@@ -8,7 +8,10 @@ const MyData = require("../src/models/MyData");
 const User = require("../src/models/User");
 
 const MIN_COST = 10000;
-const MAX_COST = 10000000;
+const MAX_COST = 9000000;
+
+const MIN_RATE = 0.1;
+const MAX_RATE = 0.3;
 
 function assignRandomValues(obj) {
   for (let key in obj) {
@@ -17,21 +20,29 @@ function assignRandomValues(obj) {
     } else if (typeof obj[key] === "boolean") {
       obj[key] = Math.random() < 0.5 ? true : false;
     } else if (typeof obj[key] === "number") {
-      obj[key] =
-        Math.floor(
-          (Math.random() * (MAX_COST - MIN_COST + 1) + MIN_COST) / 100
-        ) * 100;
+      if (key === "taxPaidRate") {
+        const taxPaidRate = Math.random() * (MAX_RATE - MIN_RATE) + MIN_RATE;
+        obj[key] = taxPaidRate;
+        console.log("rate: ", taxPaidRate);
+      } else {
+        obj[key] =
+          Math.floor(
+            (Math.random() * (MAX_COST - MIN_COST + 1) + MIN_COST) / 100
+          ) * 100;
+      }
     }
   }
 }
 
 router.post("/create", async function (req, res, next) {
   try {
+    console.log("create");
     const { userId } = req.body;
 
     let obj = JSON.parse(JSON.stringify(MyDataJsonFile));
-
     assignRandomValues(obj);
+
+    console.log("obj: ");
 
     const user = await User.findById(userId);
     if (user.myData) {
@@ -46,7 +57,7 @@ router.post("/create", async function (req, res, next) {
 
     res.send({ updatedUser, createdMyData });
   } catch (err) {
-    res.send(err);
+    res.send("err: " + err);
   }
 });
 
