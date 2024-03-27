738399c175d400941fe5759227d2df90e6c765c5
youhyeoneee <khukyh2018@khu.ac.kr>
2024-03-27 14:10:40 +0900

    fix: user 스키마에 default null 추가

--- src/models/User.js
+++ src/models/User.js
@@ -6,10 +6,10 @@ const userSchema = new mongoose.Schema({
   resultArray: [{ type: Number }],
   likedEtf: [{ type: String }],
   likedFund: [{ type: String }],
-  birthday: { type: String },
-  email: { type: String },
-  salary: { type: Number },
-  address: { type: Number },
+  birthday: { type: String, default: null },
+  email: { type: String, default: null },
+  salary: { type: Number, default: null },
+  address: { type: Number, default: null },
 });
 
 const User = mongoose.model("User", userSchema);
